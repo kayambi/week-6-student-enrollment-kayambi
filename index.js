21912470cd2acac39d94db243cd1babc50315c14
kayambi <you@example.com>
2020-07-18 22:45:51 -0700

    added few lines on evennt button

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let addCoursesBtn = document.getElementById("addbtn");
 let edditInfBtn = document.getElementById("Editbtn"); 
 
 let students;
+let courses;
 
 function fetchData(url) {
     return fetch(url).then(res => res.json())
@@ -26,62 +27,69 @@ function checkStatus(response){
 }
 
 
-   //   Add Function // 
 
-   function addCourses(){
+   //   Add function to add display courses and adding them
+   // Add EvenListiner to addbtn courses . once  the click the button , available data should be displayed then put option to shooses 
+  // what daya to select. 
 
-    return  '';
+    function addCoursesToStudent(student,course) { 
 
-} 
+        return `Hello `
+        
+        const targetStudent = students.find((s) => s.id === student.id);
+        targetStudent.courses.push(course);
+        studentList.innerHTML = students.map(studentToList).join('')
+     
+ } 
+
+//  function renderedStudents(){
+
+//     // toDO
+
+//  }
 
 
 //  Edit function  
 
-function editInformation(){
+function editInformation(student, firstName, lastName){
 
-return "hello edit"
+return "Holla"
 }
 
-
-function changeStatusColor(){ 
-
-if( status === true ){
-
-   return status.style="red";
-
-}else{
-
-   return status.style= "green"
+//  Change status color to be green if the status is true,  
+// Change color color to be red if the status is false;
+function changeStatusColor(student){ 
+    let x="green";
+    let y="red";
+    return (student === true ? x : y);
+    
 }
-} 
 
 //  Function to map my data
+
+// StudentLIst function
 
 function studentToList(student){ 
 
 return `
-        <div class="container">
+        <li class="container">
                 <div align-items><ul class="list-group list-group=vertical-lg">
                     <li class="list-group-item display-inline  border border-primary">
-                     <div><span>${student.name} ${student.last_name} ${changeStatusColor(student.status)}</span>       
-                    </div><hr>
-
-                                                                 
-                    <button type="button" class="btn btn-outline-info " id="addbtn">Add Courses:${addCourses()}</button>
-                    
+                     <div>${student.name} ${student.last_name} ${changeStatusColor(student.status)}</div>                                               
+                    <button type="button" class="btn btn-outline-info " id="addbtn">AddCourses:${addCoursesToStudent()}</button>
                     <button type="button" class="btn btn-outline-info " id="Editbtn">Edit Info:${editInformation()}</button>
                     </form>
                    </li> 
               </ul></div>
             </div>
             </div>
-        </div> 
+        </li> 
 
 `;
 
-
 }
 
+//  Fetching student data
 
 function mystudentData() {
 
@@ -96,10 +104,9 @@ function mystudentData() {
      fetchData(`https://code-the-dream-school.github.io/JSONStudentsApp.github.io/Students.json`)
      .then(data =>{ 
 
+        // console.log(data)
+
         students = data;
-
-    
-
         studentList.innerHTML = `
         ${data.map(studentToList).join('')}
         `;     
@@ -107,7 +114,6 @@ function mystudentData() {
     });
 
 }
-
 
 //  Fetch Courses Data 
 
@@ -122,40 +128,43 @@ function coursesData(){
             stuCourses.style.display ='none';
         }
         
-    
+   // function that collect courses data and map it into the DOM 
+   function CoursestoList(course){
+        
+    return `
+            <div class="container">
+                    <div row align-items -start ><ul class="list-group list-group=vertical-lg">
+                        <li class="list-group-item display-inline  border border-primary">
+                         <div class ="col">${course.name} ${course.duration} 
+                         </div><hr>
+                         <button type="button" class="btn btn-outline-info " id="addbtn">Add Course</button>
+                       </li> 
+                  </ul></div>
+                </div>
+                </div>
+            </div>         
+    `;
+
+}
+
 fetchData(`https://code-the-dream-school.github.io/JSONStudentsApp.github.io/Courses.json`)
 .then(data =>  {
-    // function that collect courses data and map it into the DOM 
-            function CoursestoList(course){ 
-
-            return `
-                    <div class="container">
-                            <div row align-items -start ><ul class="list-group list-group=vertical-lg">
-                                <li class="list-group-item display-inline  border border-primary">
-                                 <div class ="col">${course.name} ${course.duration} 
-                                 </div><hr>
-                                 <button type="button" class="btn btn-outline-info " id="addbtn">Add Course</button>
-                               </li> 
-                          </ul></div>
-                        </div>
-                        </div>
-                    </div>         
-            `;
-        
-        }
-     
-CoursesList.innerHTML = `
-        ${data.map(CoursestoList).join('')}
-        `; 
+        // console.log(data)
+        // store data into the courses variables
+            courses = data
+            CoursesList.innerHTML = `
+                    ${data.map(CoursestoList).join('')}
+                    `; 
 });
     
 }
-
-
     //  Add EventListiner
 
 studentsBtn.addEventListener('click',mystudentData);
 coursesBtn.addEventListener('click',coursesData);
+
+
+
 
 
 
