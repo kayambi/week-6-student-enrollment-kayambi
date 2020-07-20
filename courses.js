d7b69dcd301725086a492215b6daca75f9f4742a
kayambi <you@example.com>
2020-07-19 23:31:57 -0700

    setting up LocalStorage & worked on stbtn

--- courses.js
+++ courses.js
@@ -1,118 +1,75 @@
+ let StudentButton = document.querySelector('.student');
+ let studentD = document.querySelector('.user');
 
-// variables for students and information for students 
-let studentsBtn = document.getElementById('students');
-let user = document.getElementById('user');
-let coursesBtn = document.getElementById('courses');
-let course = document.getElementById('stuCourses');
-let newStudentBtn = document.getElementById('new_student');
 
-function fetchData(url) {
+function fetchData(url){
     return fetch(url)
-    .then(checkStatus)
-    .then(res => res.json())
-    .catch(error => console.log("it looks like they were a problem", error))
-}
-
-Promise.all([
-
-    fetchData(`https://code-the-dream-school.github.io/JSONStudentsApp.github.io/Students.json`),
-    fetchData(`https://code-the-dream-school.github.io/JSONStudentsApp.github.io/Courses.json`)
-
-])
-.then(data => {
-      console.log(data)
-
-    const studentData = data[0];
-    const courseData = data[1];
-    generateStudentData(studentData);
-    generateCourseData(courseData);
-})
-
-// Function CheckStatus
-
-function checkStatus(response){
-    if (response.ok){
-        return Promise.resolve(response);
-    }else{
-        return Promise.reject(new Error(response.statusText));
-    }
-}
-// Function Generate Student Data 
-
-studentsBtn.addEventListener('click',generateStudentData)
+         .then(res => res.json())
+     }
 
 
-//    Hide and Show content when the user click the button
-            if (user.style.display =='none') {
-                user.style.display = 'block';
 
-            }else {
-                user.style.display ='none';
+fetchData("https://code-the-dream-school.github.io/JSONStudentsApp.github.io/Courses.json")
+    .then(data => {
+        
+        // set the storage for courses data
+         localStorage.setItem("courses", JSON.stringify(data));
 
-            }
+        //  convert data ito json object
 
-    //  looping through the object lists 
-                students = data;
-            for (let i=0 ; i<data.length ; i++){
+        console.log(data)
+    })
+    
+fetchData("https://code-the-dream-school.github.io/JSONStudentsApp.github.io/Students.json")
+    .then(data =>{
 
-                studentInfoList(data[i]);
-            }
+        // set the storage for students data 
 
-            //  function that write studenttoList
-        function studentInfoList(student) {
+        localStorage.setItem("students", JSON.stringify(data));
+        // convert the data into json Object
+        console.log(data)
+    })
+    
 
-            // let user = document.getElementById('user');
-            let myHTML = `<li>${student.name} ${student.last_name}`;
-                         if(student.status === true) {
-                             myHTML += `<span class="active"></span>`;
-                         }else{
-                            myHTML += `<span class="nonactive"></span>`;
-                         }
-                          myHTML +=`<div class= "mt-4"><div>`;
-                          myHTML +=`<button type="button" class= "btn btn-outline-info" id="addbtn" >Add Course<button>`;
-                          myHTML +=`<button type="button" class= "btn btn-outline-info" id="addbtn" >Edit Information<button>`;
+//  Write a function that will get students data from local storage and 
+// dispay them to the DOM
 
-                myHTML += `</div></li>`;
-                user.innerHTML = myHTML;
 
-                            
+    StudentButton.addEventListener('click',function(){
+        
+        function getStudents(student){ 
+
+            let  x= localStorage.getItem("students");
+            studentD.innerHTML = x;   
+        
         }
-      
-    const html = data.map(data =>{
-        return `
-        <table class ="table" >
-        <td>${data.name}</td>
-        <td>${data.last_name}</td>
-        <td>${data.status}</td>  
-        
-        </table>
-        `;     
 
-        }).join('');
-        console.log(html)
-        document.getElementById('user').insertAdjacentHTML("beforeend",html)
 
-// generate Courses Data 
-// AddEvent Listiner
+    })
 
-coursesBtn.addEventListener('click',generateCourseData)
-function  generateCourseData (data){
+ 
 
-//  create a variable to store HTML
+//   write a function that will get courses
 
-    let li =`<tr>
-    <th>Name</th>
-     <th>Duration</th>
-                </tr>`;  
-        // loop through each data and add a table row
+     function getCourse(course){
 
-        for (let i=0; i<data.length ; i++){
-        li +=`<tr>
+        //  toDO
 
-        <td>${data.name}</td>
-        <td>${data.duration}</td>
-        </tr>`;
-           }
-            user.innerHTML = li ;
-            }
-        
+
+    }
+
+
+// let  myObject={
+//     name:"kay",
+//     age:34
+// };
+
+// let myObject_serialize  = JSON.stringify(myObject)
+// localStorage.setItem('myObject',myObject_serialize);
+
+//  START USING OUR Object 
+
+// let myObject_Deserialize = JSON.parse(localStorage.getItem('myObject))
+
+
+
